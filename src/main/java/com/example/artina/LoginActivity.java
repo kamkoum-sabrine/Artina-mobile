79f7c93906e2df8887a3c4e406cbfe01ac3e418b
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2025-04-29 00:54:28 +0100

    create login page

--- src/main/java/com/example/artina/LoginActivity.java
+++ src/main/java/com/example/artina/LoginActivity.java
@@ -15,10 +15,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        findViewById(R.id.btn_login).setOnClickListener(v -> {
+            // Logique de connexion
         });
     }
 }
